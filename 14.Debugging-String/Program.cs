bf18fd39ce05d74e5cf1742fc9cff0af523d0355
KalinVasilevSV <kalinsv@abv.bg>
2019-01-13 19:27:50 +0200

    Evening work 13.01.2019

--- 14.Debugging-String/Program.cs
+++ 14.Debugging-String/Program.cs
@@ -6,7 +6,36 @@ namespace _14.Debugging_String
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string text = Console.ReadLine();
+            int jump = int.Parse(Console.ReadLine());
+
+            const char Search = 'p';  // This was typed with a cyrillic symbol ASCII 1088 instead of the english ASCII 112
+            bool hasMatch = false;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == Search)
+                {
+                    hasMatch = true;
+
+                    int toPrint = jump + 1;
+                    int endIndex = i + toPrint;
+
+                    if (endIndex > text.Length)
+                    {
+                        toPrint = text.Length-i;
+                    }
+
+                    string matchedString = text.Substring(i, toPrint);
+                    Console.WriteLine(matchedString);
+                    i += jump;
+                }
+            }
+
+            if (!hasMatch)
+            {
+                Console.WriteLine("no");
+            }
         }
     }
 }
---
+++ 15.Debugging-Instruction-Set/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _15.Debugging_Instruction_Set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opCode = Console.ReadLine();
+
+            while (opCode != "END")
+            {
+                string[] codeArgs = opCode.Split(' ');
+                int currentArg = 0;
+
+                long result = 0;
+                switch (codeArgs[0])
+                {
+                    case "INC":
+                        {
+                            long operandOne = long.Parse(codeArgs[1]);
+                            result = ++operandOne;
+                            break;
+                        }
+                    case "DEC":
+                        {
+                            long operandOne = long.Parse(codeArgs[1]);
+                            result = --operandOne;
+                            break;
+                        }
+                    case "ADD":
+                        {
+                            long operandOne = long.Parse(codeArgs[1]);
+                            long operandTwo = long.Parse(codeArgs[2]);
+                            result = operandOne + operandTwo;
+                            break;
+                        }
+                    case "MLA":
+                        {
+                            long operandOne = long.Parse(codeArgs[1]);
+                            long operandTwo = long.Parse(codeArgs[2]);
+                            result = operandOne * operandTwo;
+                            break;
+                        }
+                    default:break;
+                }
+
+                Console.WriteLine(result);
+
+                opCode = Console.ReadLine();
+            }
+        }
+    }
+}
